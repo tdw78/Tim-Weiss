3d55cbd7cabdedaa5bb281689a01be69795fba45
Tim Weiss <tdweiss1978@gmail.com>
2020-05-29 22:40:17 -0700

    About Complete

--- src/components/About.js
+++ src/components/About.js
@@ -4,21 +4,25 @@ import React, { Component } from 'react';
 class About extends Component {
 
   render () {
+    const titleStyle = {
+      fontSize: 42,
+      paddingBottom: 22,
+      paddingTop: 33
+    }
+
     return (
       <div>
         <div className="main">
-          <h2>About Me</h2>  
+          <h2 style={titleStyle}>About Me</h2>  
           <div class="row">
             <div class="col-lg-3"></div>
             <div class="col-lg-6" id="about-me">
               I am a full stack web developer in San Jose, California.  I have always been
-              interested in computers and technology and when I began to learn more about web development,
-              I fell in love immediately.  I love the cognitive aspect of imagination and problem solving.
-              I am passionate about building applications and continuing to sharpen my development skills.
-              I was not always involved in web development.  I grew up around the game of golf.  I played golf in high school and then in
-              college at the Univeristy of Arizona.  After graduation from the U of A, I attempted to play
-              golf for a living.  When that didn't pan out, I wanted to stay close to the game, so I become
-              a PGA professional.  I was a pro for 12 years before switching careers. 
+              interested in computers and technology and when I began to study web development,
+              I fell in love immediately.  I am passionate about building applications and continuing to sharpen my development skills.
+              However, I was not always involved in web development.  I grew up around the game of golf.  I played golf in high school and then in
+              college at the Univeristy of Arizona.  After graduation from the U of A, I wanted to stay close to the game, so I become
+              a PGA professional.  I was a PGA professional for 12 years before switching careers. 
             </div>
             <div class="col-lg-3"></div>
           </div>
